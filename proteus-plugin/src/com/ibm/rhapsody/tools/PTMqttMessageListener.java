ccd4f568d236dd919de6bf5046a3e67a2db4ad82
Thiker <4900976+thiker@users.noreply.github.com>
2023-06-10 18:33:21 +0200

    bidirectional communication

--- proteus-plugin/src/com/ibm/rhapsody/tools/PTMqttMessageListener.java
+++ proteus-plugin/src/com/ibm/rhapsody/tools/PTMqttMessageListener.java
@@ -7,6 +7,7 @@ import com.telelogic.rhapsody.core.IRPApplication;
 import com.telelogic.rhapsody.core.IRPDiagram;
 import com.telelogic.rhapsody.core.IRPModelElement;
 import com.telelogic.rhapsody.core.IRPProject;
+import com.telelogic.rhapsody.core.IRPStatechart;
 import com.telelogic.rhapsody.core.RhapsodyAppServer;
 
 public class PTMqttMessageListener implements IMqttMessageListener {
@@ -14,19 +15,18 @@ public class PTMqttMessageListener implements IMqttMessageListener {
 	@Override
 	public void messageArrived(String topic, MqttMessage msg) throws Exception {
 		
-		javax.swing.JOptionPane.showMessageDialog(null, "MQTT Message Received" + msg.toString() + " with topic" + topic, "ProteusExtractor", javax.swing.JOptionPane.PLAIN_MESSAGE);
 		IRPApplication rhpApp = RhapsodyAppServer.getActiveRhapsodyApplication();
 		IRPProject rhpProj = rhpApp.activeProject();
-		
 		IRPModelElement modelElement = rhpProj.findElementByGUID(msg.toString());
+		
 		if (modelElement != null) {
-			javax.swing.JOptionPane.showMessageDialog(null, "Opening diagram with metaclass" + modelElement.getMetaClass(), "ProteusExtractor", javax.swing.JOptionPane.PLAIN_MESSAGE);
+			//modelElement.openFeaturesDialog(1);
 			if (modelElement.getMetaClass().equals("ObjectModelDiagram")) {
-				javax.swing.JOptionPane.showMessageDialog(null, "Opening diagram", "ProteusExtractor", javax.swing.JOptionPane.PLAIN_MESSAGE);
+				((IRPDiagram)modelElement).openDiagram();
+			} else if (modelElement.getMetaClass().equals("Statechart")) {
+				((IRPStatechart)modelElement).getStatechartDiagram().openDiagram();
+				
 			}
-		} else {
-			javax.swing.JOptionPane.showMessageDialog(null, "Opening diagram but not found", "ProteusExtractor", javax.swing.JOptionPane.WARNING_MESSAGE);
-			
 		}
 	}
 
